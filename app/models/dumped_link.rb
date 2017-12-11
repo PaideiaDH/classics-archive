4a34738016ee62daea5f1604e227bb143f2cc8d6
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-12-10 18:13:14 -0800

    dumped link publication

--- app/models/dumped_link.rb
+++ app/models/dumped_link.rb
@@ -3,7 +3,7 @@ class DumpedLink < ActiveRecord::Base
   attr_accessor :publication
 
   def publication
-    uri = URI.parse(d.url)
+    uri = URI.parse(url)
     host = uri.host
     parsed_host = host.gsub(/^www./, '').split('.')[0]
     parsed_host
